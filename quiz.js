c2cf416ea8739249ff5eabff498a7196808dd6a3
Charles Effiong <charleseffiong28@gmail.com>
2025-01-27 13:44:05 +0100

    quiz

--- quiz.js
+++ quiz.js
@@ -3,3 +3,9 @@ function checkAnswer() {
 };
 const selectedRadioButton = document.querySelector('input[name="quiz"]');
 const userAnswer = document.querySelector(['checked, value = 4' ]);
+
+if (correctAnswer === userAnswer){
+    document.getElementById('#feedback').textContent = "Correct! well done";
+} else {
+    document.getElementById('#feedback').textContent = "That is incorrect, Try again"
+}
