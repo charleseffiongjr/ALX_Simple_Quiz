0ad67f7161d096076efaa8debbb4c90faf32f1a9
Charles Effiong <charleseffiong28@gmail.com>
2025-01-27 13:18:34 +0100

    quiz

--- quiz.js
+++ quiz.js
@@ -1,4 +1,5 @@
 function checkAnswer() {
     const correctAnswer = '4';
 };
-const selectedRadioButton = document.querySelector('input[name="quiz"])
+const selectedRadioButton = document.querySelector('input[name="quiz"]');
+const userAnswer = document.querySelector(['checked, value = 4' ]);
